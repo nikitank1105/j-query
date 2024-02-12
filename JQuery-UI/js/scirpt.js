ffe949b38e92a5ebc06bf7ac4567563a43ee2df2
nikitank <159780820+nikitank1105@users.noreply.github.com>
2024-02-12 17:48:51 +0530

    j
    J

---
+++ JQuery-UI/js/scirpt.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $(function() {
+        $("input").checkboxradio();
+    });
+});
+
+$(function() {
+    $("#datepicker").datepicker();
+})
+$(function() {
+    $("#slider").slider();
+});
